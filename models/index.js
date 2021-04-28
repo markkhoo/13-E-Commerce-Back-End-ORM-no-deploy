ee16ebcad40bfb87d2186eaef2c06dc4243dfaab
markeezy <markkhoo95@gmail.com>
2021-04-28 00:06:21 -0700

    complete index for models

--- models/index.js
+++ models/index.js
@@ -6,19 +6,19 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
+  foreignKey: 'category_id',
 });
 // Categories have many Products
 Category.hasMany(Product, {
-
+  foreignKey: 'category_id',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsTo(ProductTag, {
-
+  foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsTo(ProductTag, {
-
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
