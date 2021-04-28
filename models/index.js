3a23894ef3d8240c506bed086d0b325e3a0b358c
markeezy <markkhoo95@gmail.com>
2021-04-27 23:51:00 -0700

    complete models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
 
+});
 // Categories have many Products
+Category.hasMany(Product, {
 
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(ProductTag, {
 
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(ProductTag, {
+
+});
 
 module.exports = {
   Product,
